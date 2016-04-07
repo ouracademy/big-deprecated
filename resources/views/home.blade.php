2576cce67660142ef20abe35c8494bbfe9d81b66
Arthur Mauricio <amd11dot4@gmail.com>
2016-04-07 17:24:11 +0000

    Updated favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div class="col-md-offset-1 col-md-4 text-center">
                 <div class="thumbnail service">
-                    <img class="img-responsive" src="https://d30y9cdsu7xlg0.cloudfront.net/png/117741-200.png" height="320px" width="70%" alt="">
+                    <img class="img-responsive" src="https://d30y9cdsu7xlg0.cloudfront.net/png/117741-200.png" height="320px" width="50%" alt="">
                     <div class="caption">
                         <h3>Cursos</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe et quisquam nesciunt maxime.</p>
@@ -22,7 +22,7 @@
             </div>
             <div class="col-md-offset-1 col-md-4 text-center">
                 <div class="thumbnail service">
-                    <img class="img-responsive" src="http://ca-helpline.com/wp-content/uploads/2015/11/classes-icon.png" height="320px" width="70%" alt="">
+                    <img class="img-responsive" src="http://ca-helpline.com/wp-content/uploads/2015/11/classes-icon.png" height="320px" width="50%" alt="">
                     <div class="caption">
                         <h3>Talleres y seminarios</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe et quisquam nesciunt maxime.</p>
